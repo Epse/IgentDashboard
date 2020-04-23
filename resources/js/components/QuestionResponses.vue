1876c5f61b1111d453127ec0e51b7c381dc89d20
Stef Pletinck <s@stefpletinck.be>
2020-04-23 11:04:52 +0200

    Fix #12 by polling the server for new responses

--- resources/js/components/QuestionResponses.vue
+++ resources/js/components/QuestionResponses.vue
@@ -37,70 +37,88 @@
 </template>
 
 <script>
-    const axios = require('axios').default;
-    import * as d3 from 'd3';
+const axios = require("axios").default;
+import * as d3 from "d3";
+import orderBy from 'lodash/orderBy';
 
-    export default {
-        props: {
-            question: {
-                type: Object,
-            },
-        },
-        data: function () {
-            return {
-                answers: [],
-            };
-        },
-        watch: {
-            question: function(newQ, oldQ) {
-                this.pullAnswers(newQ);
-            },
-        },
-        created: function() {
+export default {
+    props: {
+        question: {
+            type: Object
+        }
+    },
+    data: function() {
+        return {
+            answers: [],
+            polling: null,
+        };
+    },
+    watch: {
+        question: function(newQ, oldQ) {
+            this.pullAnswers(newQ);
+        }
+    },
+    created: function() {
+        this.pullAnswers(this.question);
+        this.polling = setInterval(() => {
             this.pullAnswers(this.question);
-        },
-        computed: {
-            aggregates () {
-                return this.answers.reduce(function (carry, x) {
-                    let el = carry.find(r => r && r.key === x.response);
-                    if (el) {
-                        el.count += 1;
-                    } else {
-                        carry.push({ key: x.response, count: 1});
-                    }
-                    return carry;
-                }, []);
-            },
-            mean () {
-                return d3.mean(this.answers, d => d.response);
-            },
-            median () {
-                return d3.median(this.answers, d => d.response);
-            },
-            deviation () {
-                return d3.deviation(this.answers, d => d.response);
-            },
-        },
-        methods: {
-            pullAnswers: function(q) {
-                // This is a method because axios.get is async.
-                if (q == null) {
-                    return;
+        }, 5000 + Math.floor(Math.random() * 1000)); // Spread the delays and load
+    },
+    beforeDestroy () {
+        clearInterval(this.polling);
+    },
+    computed: {
+        aggregates() {
+            return this.answers.reduce(function(carry, x) {
+                let el = carry.find(r => r && r.key === x.response);
+                if (el) {
+                    el.count += 1;
+                } else {
+                    carry.push({ key: x.response, count: 1 });
                 }
+                return carry;
+            }, []);
+        },
+        mean() {
+            return d3.mean(this.answers, d => d.response);
+        },
+        median() {
+            return d3.median(this.answers, d => d.response);
+        },
+        deviation() {
+            return d3.deviation(this.answers, d => d.response);
+        }
+    },
+    methods: {
+        pullAnswers: function(q) {
+            // This is a method because axios.get is async.
+            if (q == null) {
+                return;
+            }
 
-                axios.get(`/questions/${q.id}/answers`)
-                     .then(res => {
-                         this.answers = res.data;
-                     });
-            },
-        },
-        components: {
-            'vertical-bar-chart': require('./charts/VerticalBarChart.vue').default,
-            'yes-no-chart': require('./charts/YesNoChart.vue').default,
-        },
-    };
+            if (this.answers.length == 0) {
+                axios.get(`/questions/${q.id}/answers`).then(res => {
+                    this.answers = res.data;
+                });
+            } else {
+                const sorted = orderBy(this.answers, [o => o.created_at], ["desc"]);
+                const lastDate = sorted[0].created_at;
+                axios.get(`/questions/${q.id}/answers`, {
+                    params: {
+                        since: lastDate,
+                    }
+                }).then(res => {
+                    this.answers = this.answers.concat(res.data);
+                });
+            }
+
+        }
+    },
+    components: {
+        "vertical-bar-chart": require("./charts/VerticalBarChart.vue").default,
+        "yes-no-chart": require("./charts/YesNoChart.vue").default
+    }
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
