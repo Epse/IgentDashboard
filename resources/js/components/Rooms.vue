009fff2f9b385ac68e74f5760b94057d7c5336cc
Stef Pletinck <s@stefpletinck.be>
2020-05-08 12:54:24 +0200

    Load appropriate sensor data into rooms vis

--- resources/js/components/Rooms.vue
+++ resources/js/components/Rooms.vue
@@ -1,14 +1,30 @@
 <template>
-    <div class="row">
-        <div class="col">
-            <svg :id="uid" width="100%"></svg>
+    <div>
+        <div class="row">
+            <div class="col">
+                <label for="">Type sensor</label>
+                <select v-model="sensorType">
+                    <option value="-1" selected disabled>Kies een sensortype</option>
+                    <option
+                        :value="type.id"
+                        v-for="type in sensorTypes"
+                        :key="type.id"
+                        >{{ type.type }}</option
+                    >
+                </select>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <svg :id="uid" width="100%"></svg>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
 import * as d3 from "d3";
-const axios = require('axios').default;
+const axios = require("axios").default;
 
 let _uid = 0;
 
@@ -16,14 +32,15 @@ const img_width = 1653;
 const img_height = 1169;
 
 export default {
-    data () {
+    data() {
         return {
             rooms: {},
+            sensorType: -1,
+            sensorTypes: [],
+            data: []
         };
     },
-    props: {
-
-    },
+    props: {},
     computed: {
         roomArray: function() {
             let ra = [];
@@ -33,62 +50,107 @@ export default {
                     x1: value.x1,
                     x2: value.x2,
                     y1: value.y1,
-                    y2: value.y2,
+                    y2: value.y2
                 });
             }
             return ra;
+        }
+    },
+    watch: {
+        data: function(val, old) {
+            this.redraw();
         },
+        sensorType: function(val, old) {
+            if (val == NaN || val <= 0) {
+                return;
+            }
+
+            axios
+                .get("/sensors", {
+                    params: {
+                        type: val
+                    }
+                })
+                .then(res => {
+                    this.data = [];
+                    res.data.forEach(sensor => {
+                        let el = this.data.find(r => r.room == sensor.room);
+                        if (!el) {
+                            this.data.push({ room: sensor.room, values: [] });
+                        }
+                        el = this.data.find(r => r.room == sensor.room);
+                        axios
+                            .get(`/sensors/${sensor.id}/data`)
+                            .then(res =>
+                                res.data.forEach(datapoint => {
+                                    el.values.push(datapoint.value);
+                                })
+                            )
+                            .catch(err => console.error(err));
+                    });
+                })
+                .catch(err => console.error(err));
+        }
     },
     methods: {
-        redraw: function () {
+        getTypes: function() {
+            axios
+                .get("/sensortypes")
+                .then(res => this.sensorTypes = res.data)
+                .catch(err => console.error(err));
+        },
+        redraw: function() {
             const svg = d3.select(`svg#${this.uid}`);
-            const width = svg.node()
-                             .getBoundingClientRect()
-                             .width;
+            const width = svg.node().getBoundingClientRect().width;
 
             const scaling = width / img_width;
 
-            svg.append('svg:image')
-               .attr('x', 0)
-               .attr('y', 0)
-               .attr('width', width)
-               .attr('xlink:href', '/img/floorplan_10th.png');
+            // TODO: colour mapping for all sensor types
 
-            svg.attr('height', scaling * img_height);
+            svg.append("svg:image")
+                .attr("x", 0)
+                .attr("y", 0)
+                .attr("width", width)
+                .attr("xlink:href", "/img/floorplan_10th.png");
 
-            svg.append('g')
-               .selectAll('a')
-               .data(this.roomArray)
-               .join('svg:a')
-               .attr('x', d => scaling * d.x1)
-               .attr('y', d => scaling * d.y1)
-               .attr('xlink:href', d => this.roomUrl(d.name))
-               .append('rect')
-               .attr('x', d => scaling * d.x1)
-               .attr('y', d => scaling * d.y1)
-               .attr('width', d => scaling * (d.x2 - d.x1))
-               .attr('height', d => scaling * (d.y2 - d.y1))
-               .attr('fill', 'rgba(10,10,10,0.4)');
+            svg.attr("height", scaling * img_height);
 
+            svg.append("g")
+                .selectAll("a")
+                .data(this.roomArray)
+                .join("svg:a")
+                .attr("x", d => scaling * d.x1)
+                .attr("y", d => scaling * d.y1)
+                .attr("xlink:href", d => this.roomUrl(d.name))
+                .append("rect")
+                .attr("x", d => scaling * d.x1)
+                .attr("y", d => scaling * d.y1)
+                .attr("width", d => scaling * (d.x2 - d.x1))
+                .attr("height", d => scaling * (d.y2 - d.y1))
+                .attr("fill", "rgba(10,10,10,0.4)");
         },
         roomUrl(room_name) {
             return `/rooms/${room_name}`;
         },
-        pullRooms () {
-            axios.get('/img/floorplan_10th.json')
-                 .then(res => {
-                     this.rooms = res.data;
-                     this.redraw();
-                 })
-                 .catch(err => console.error(err));
-        },
+        pullRooms() {
+            axios
+                .get("/img/floorplan_10th.json")
+                .then(res => {
+                    this.rooms = res.data;
+                    this.redraw();
+                })
+                .catch(err => console.error(err));
+        }
     },
     beforeCreate() {
         this.uid = `rooms-${_uid}`;
         _uid += 1;
     },
+    beforeMount() {
+        this.getTypes();
+    },
     mounted() {
         this.pullRooms();
-    },
-}
+    }
+};
 </script>
